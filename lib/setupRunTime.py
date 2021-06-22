100a06cd8e96c2951fc198e34f2a59c6a6222fa1
Jim Blakley <jblake1@andrew.cmu.edu>
2021-06-22 06:49:01 -0400

    Enable gpu in k8s

--- lib/setupRunTime.py
+++ lib/setupRunTime.py
@@ -71,6 +71,10 @@ def setupKubernetes(cnf):
          
         # Restart docker daemon
         oscmd("sudo systemctl restart docker")
+        
+        entry = input("Enable NVIDIA GPU in Kubernetes? [y/N] ") or "n"
+        if entry in ['Y','y']:
+            oscmd("kubectl create -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/1.0.0-beta4/nvidia-device-plugin.yml")
     return 0
 
 
