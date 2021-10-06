765221c1deac01524c011994dc140b2049b99fba
jblake1 <jblake1@jrb-pyedgesim-vm2.internal.cloudapp.net>
2021-10-06 15:37:24 +0000

    fixes for k9s install

--- install_requirements.py
+++ install_requirements.py
@@ -8,12 +8,12 @@ oscmd("sudo apt update")
 ''' Java '''
 oscmd("sudo apt install default-jdk -y")
 oscmd("sudo apt install openjdk-8-jre -y")
-
+oscmd("sudo apt install -y jq influxdb-client webpack npm")
 ''' K9s '''
-
-oscmd("sudo apt install -y linuxbrew-wrapper jq influxdb-client webpack npm")
-oscmd("brew install k9s")
-oscmd("echo alias k9s=\'$HOME/.linuxbrew/bin/k9s\' >> ~/.bashrc")
+BREWPATH = "/home/linuxbrew/.linuxbrew/bin"
+oscmd('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+oscmd("{}/brew install k9s".format(BREWPATH))
+oscmd("echo alias k9s=\'{}/k9s\' >> ~/.bashrc".format(BREWPATH))
 
 pipsetup="pip install requests;pip install -r pip_requirements.txt"
 setupclient="python lib/setupAutomation.py"
