27a34d5283e8e0c285615163d2c9eedbf5084edd
Jim Blakley <jimbla9@gmail.com>
2021-10-21 12:33:46 -0400

    Remove duplicate brew/k9 install

--- install_requirements.py
+++ install_requirements.py
@@ -9,11 +9,6 @@ oscmd("sudo apt update")
 oscmd("sudo apt install default-jdk -y")
 oscmd("sudo apt install openjdk-8-jre -y")
 oscmd("sudo apt install -y jq influxdb-client webpack npm")
-''' K9s '''
-BREWPATH = "$HOME/.linuxbrew/bin"
-oscmd('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
-oscmd("{}/brew install k9s".format(BREWPATH))
-oscmd("echo alias k9s=\'{}/k9s\' >> ~/.bashrc".format(BREWPATH))
 
 pipsetup="pip install requests;pip install -r pip_requirements.txt"
 setupclient="python lib/setupAutomation.py"
